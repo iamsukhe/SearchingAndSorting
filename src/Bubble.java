2f3b6ec0cda0300f0b51666cb463ee5e0c5b0cc7
Sukhvinder Singh <sukhe353@gmail.com>
2021-12-26 23:35:10 +0530

    feat : added Selections Sort

--- src/Bubble.java
+++ src/Bubble.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Bubble {
     public static void main(String[] args) {
-        int [] array = {1,2,3};
+        int [] array = {1,2,3,1,2};
         bubble(array);
 
         System.out.println(Arrays.toString(array));
---
+++ src/Selection.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,1,2};
+        selection(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+    public static void selection(int[] arr ){
+         for(int i=0; i< arr.length; i++){
+             int last = arr.length-1-i;
+             int maxIndex = maxIndex(arr,0,last);
+             swap(arr, maxIndex, last);
+         }
+    }
+
+    public static void swap(int[] arr, int first, int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+
+
+    public static int maxIndex(int[] arr, int s, int e) {
+        int max = s;
+        for(int i=0; i<= e ; i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+
+}
